091c37d861e0ef00bf60a241ca301aa7223bc27f
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-10 18:54:44 +0100

    Random posities v1

--- Schip.java
+++ Schip.java
@@ -5,21 +5,21 @@ public class Schip {
 	private int aantalRaak; 
 	
 	public void setLocaties(int[] loc) {
-		locaties = loc;
+		locaties = loc; //loc staat gelijk aan de opgehaalde locaties
 	}
 	
-	public String checkInvoer(String gebruikersInvoer) {
+	public String checkInvoer(String gebruikersInvoer) { //checkInvoer die gebruikersInvoer verwacht als String
 		
 		String resultaat = "Mis.. Beter richten!";
 		
-		int invoer = Integer.parseInt(gebruikersInvoer);
+		int invoer = Integer.parseInt(gebruikersInvoer); //invoer is de van een String naar int omgezette gebruikersInvoer
 		
 		for(int loc : locaties) {
 			
 			if (invoer == loc) {
 				aantalRaak++;
 				
-				resultaat = aantalRaak == locaties.length ? "Alles is kapot!" : "Raak! Ga zo door!";
+				resultaat = aantalRaak == locaties.length ? "Alles is kapot!" : "Raak! Ga zo door!"; //snelle if/else
 				break;
 				
 			}
--- Zeeslag.java
+++ Zeeslag.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Zeeslag {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		Schip schip = new Schip();
-		String gebruikersInvoer;
+		Scanner scanner = new Scanner(System.in); //invoer van scanner
+		Random random = new Random(); //invoer van random
+		Schip schip = new Schip(); 
+		String gebruikersInvoer; 
 		int aantalBeurten = 0;
-		boolean schipVaartNog = true;
-		int [] locaties = {3,4,6};
-		schip.setLocaties(locaties);
+		boolean schipVaartNog = true; //schip begint op true en moet naar false
+		int tijdelijk = random.nextInt(5) +1; // geeft locaties tussen 1 en 5
+		int [] locaties = {tijdelijk, ++tijdelijk, ++tijdelijk}; //zorgt dat de schepen niet op dezelfde plek liggen
+		schip.setLocaties(locaties); //op te halen in de Schip-class
 		
-		while(schipVaartNog) {
+		while(schipVaartNog) { 
 			String resultaat;
-			System.out.println("Schiet dan...");
-			gebruikersInvoer = scanner.next();
-			resultaat = schip.checkInvoer(gebruikersInvoer);
-			aantalBeurten++;
-			if(resultaat.equals("Alles is kapot!")) {
-				schipVaartNog = false;
+			System.out.println("Schiet dan..."); //zolang schip nog vaart moet je schieten
+			gebruikersInvoer = scanner.next(); 
+			resultaat = schip.checkInvoer(gebruikersInvoer); 
+			aantalBeurten++; //aantal beurten gaan omhoog
+			if(resultaat.equals("Alles is kapot!")) { //zodra resultaat gelijk staat aan de String "Alles is kapot!" is het spel klaar en worden de beurten weergegeven
+				schipVaartNog = false; //De boot zinkt
 			}
 		}
 		
