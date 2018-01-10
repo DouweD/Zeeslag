b374cbdccc1cc7abdd933ee0b36548f8ba80eef4
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-10 22:58:25 +0100

    Met ArrayList ipv vaste integers

--- Zeeslag.java
+++ Zeeslag.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -12,8 +13,14 @@ public class Zeeslag {
 		int aantalBeurten = 0;
 		boolean schipVaartNog = true; //schip begint op true en moet naar false
 		int tijdelijk = random.nextInt(5) +1; // geeft locaties tussen 1 en 5
-		int [] locaties = {tijdelijk, ++tijdelijk, ++tijdelijk}; //zorgt dat de schepen niet op dezelfde plek liggen
-		schip.setLocaties(locaties); //op te halen in de Schip-class
+		
+		//zorgt dat de schepen niet op dezelfde plek liggen
+		schip.setLocaties(); //op te halen in de Schip-class
+		
+		ArrayList<Integer> locaties = new ArrayList<Integer>(7);
+		locaties.add(tijdelijk);
+		locaties.add(++tijdelijk);
+		locaties.add(++tijdelijk);
 		
 		while(schipVaartNog) { 
 			String resultaat;
