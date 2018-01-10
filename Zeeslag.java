9dab0ead02369c47373273331612cb9010edba10
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-10 18:01:06 +0100

    Invoeren van getallen in console (Scanner)

--- Zeeslag.java
+++ Zeeslag.java
@@ -1,18 +1,30 @@
+import java.util.Scanner;
 
 public class Zeeslag {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner scanner = new Scanner(System.in);
 		Schip schip = new Schip();
-		
-		int [] locaties = {2,4,6};
-		
+		String gebruikersInvoer;
+		int aantalBeurten = 0;
+		boolean schipVaartNog = true;
+		int [] locaties = {3,4,6};
 		schip.setLocaties(locaties);
-
-		String gebruikersInvoer = "3";
 		
-		schip.checkInvoer(gebruikersInvoer);
+		while(schipVaartNog) {
+			String resultaat;
+			System.out.println("Schiet dan...");
+			gebruikersInvoer = scanner.next();
+			resultaat = schip.checkInvoer(gebruikersInvoer);
+			aantalBeurten++;
+			if(resultaat.equals("Alles is kapot!")) {
+				schipVaartNog = false;
+			}
+		}
+		
+		System.out.println("Je hebt er " + aantalBeurten + " beurten over gedaan om het schip DEAUD te maken.");
+
 	}
 
 }
